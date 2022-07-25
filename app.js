20150a41334c8e89f5f90268cb5e34e69029d9d8
Kartikay Dager <kartikaykakudager@gmail.com>
2022-07-26 02:23:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express")
+const bodyParser=require("body-parser")
+const ejs=require("ejs")
+
+
+
+
+const app=express()
+
+
+app.set("veiws engine,ejs")
+app.set(("veiws","views"))
+
+app.list(0801)
